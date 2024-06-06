1a5f0b28aa0e2e2c6d7d214b17c25e0783f15cf2
lb12138h <164012778+lb12138h@users.noreply.github.com>
2024-06-06 08:02:25 +0000

    Deploying to main from @ lb12138h/toby@e6c34d114025c3770db8a3b385766af307e7ccdb 🚀

---
+++ toby/assets/useWebConfig-By0D1mtS.js
@@ -0,0 +1,1 @@
+import{a as n,Y as r,r as t,Z as f}from"./index-DTRIon_s.js";const u=()=>{const s=n(),{type:e}=r(),[a,o]=t.useState();return t.useEffect(()=>{if(!e){s("/");return}o(f[e])},[s,e]),a};export{u};
