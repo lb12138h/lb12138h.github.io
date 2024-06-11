015ba5916cfffde5f71a95b3ecf2cd0ecfec5407
lb12138h <164012778+lb12138h@users.noreply.github.com>
2024-06-11 10:10:31 +0000

    Deploying to main from @ lb12138h/toby@ed09f6fc39a63e218964c087a40fe3868c0333c1 🚀

--- toby/assets/pageIndex-CmhKHvUk.js
+++
@@ -1,1 +0,0 @@
-import{a,u as o,r,g as n,j as i,O as u}from"./index-DzDdqEaD.js";import{u as f}from"./useWebConfig-DIt1ovOJ.js";const m=()=>{const s=a(),{config:e}=o(),t=f();return r.useEffect(()=>{!e||!t||n(t)},[t,e,s]),i.jsx(u,{})};export{m as default};
